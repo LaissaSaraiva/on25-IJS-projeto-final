6ca86a9a15d54e412eaedd5c2b93a591b80a6e85
Laissa Cunha <laissa.cunha@ed3.com.br>
2023-09-11 00:10:49 -0300

    feat: adiciona Classe MetasFinanceiras

---
+++ src/Classes/MetasFinanceiras.js
@@ -0,0 +1,66 @@
+class MetasFinanceiras {
+  meta;
+  prazoEmMeses;
+  valorTotal;
+  valorAtual;
+  valorRestante;
+  status;
+
+  static metas = [];
+
+  constructor(meta, prazoEmMeses, valorTotal) {
+    this.verificaValorValido(valorTotal)
+    this.meta = meta;
+    this.valorTotal = valorTotal;
+    this.prazoEmMeses = prazoEmMeses;
+    this.valorAtual = 0;
+    this.atualizaValorRestante();
+    this.status = "Em andamento";
+    this.constructor.metas.push(this);
+  }
+  
+  verificaValorValido(valor) {
+    if (typeof valor !== "number" || valor < 0) {
+      throw new Error("Insira um valor válido");
+    } else {
+      return true;
+    }
+  }
+
+  retornaSugestaoEconomiaMensal() {
+    const media = this.valorTotal / this.prazoEmMeses;
+
+    return `Para alcançar ${this.valorTotal} em ${this.prazoEmMeses} meses, será recomendável economizar R$${media} mensalmente. `
+  }
+
+  guardaParaMeta(valor) {
+    this.valorAtual += valor;
+    this.atualizaValorRestante();    
+    this.verificaStatusMeta();
+    
+  }
+
+  atualizaValorRestante() {
+    this.valorRestante = this.valorTotal - this.valorAtual;
+    return this.valorRestante;
+  }
+
+  verificaStatusMeta() {
+    if(this.valorAtual === this.valorTotal){
+      this.status = "Concluído"
+      return `Parabéns! Meta: '${this.meta}' alcançada com sucesso!`
+    } else {
+      const valorRestante = this.atualizaValorRestante();
+      return `Quase lá. Ainda faltam R$${valorRestante} para alcançar sua meta.`
+    }
+  }
+}
+
+const carroNovo = new MetasFinanceiras("Carro Novo", 10, 150000);
+console.log(carroNovo);
+console.log(carroNovo.retornaSugestaoEconomiaMensal());
+carroNovo.guardaParaMeta(150000);
+console.log(MetasFinanceiras.metas)
+
+
+module.exports = { MetasFinanceiras };
